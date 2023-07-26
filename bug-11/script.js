766de85c9da3a3e7d27f8223d8cad4cf6a6b5218
Hayden Hanson <hansonhayd3n@gmail.com>
2023-07-26 13:01:00 -0400

    Add Bug 11 and Add Hypothesis 14

---
+++ bug-11/script.js
@@ -0,0 +1,9 @@
+function handleClick() {
+  console.log("Button clicked!");
+}
+
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", handleClick);
+button.addEventListener("click", handleClick);
+button.addEventListener("click", handleClick);
