ec20ec46b63e49a6259fe4ef7a9c30f30a0b485b
Hayden Hanson <hansonhayd3n@gmail.com>
2023-08-04 13:18:40 -0400

    Add Example Webpack Repo

--- bug-11/script.js
+++
@@ -1,9 +0,0 @@
-function handleClick() {
-  console.log("Button clicked!");
-}
-
-const button = document.getElementById("myButton");
-
-button.addEventListener("click", handleClick);
-button.addEventListener("click", handleClick);
-button.addEventListener("click", handleClick);
